2a63f2dd13b5b2d3499c72b14bf29f8d80e878f8
Shotaro Ooki <shoutaro0726@gmail.com>
2020-04-14 19:06:20 +0900

    Commit

---
+++ blogs/blog/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Comment
+
+class CommentCreateForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('text',)
+        widget = {'text':forms.Textarea(attrs={'class': 'textarea'})}
+        
